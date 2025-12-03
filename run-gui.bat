6d1f5a3d825a6d15f945bc55c95654dd248cb4c0
Logan Gravitt <158792807+LoganGrav@users.noreply.github.com>
2025-12-02 20:55:42 -0800

    Fix run-gui.bat to compile properly
    only some models and enums were being compiled, changes them to *.java to avoid compilation errors.

--- run-gui.bat
+++ run-gui.bat
@@ -22,7 +22,7 @@ mkdir "build\com\parkinglotmanager\util" 2>nul
 
 REM Compile enums
 echo Compiling enums...
-javac -cp ".;%MYSQL_JAR%" --release 8 -Xlint:-options -d build src\com\parkinglotmanager\enums\VehicleType.java src\com\parkinglotmanager\enums\SlotType.java src\com\parkinglotmanager\enums\VehicleMake.java src\com\parkinglotmanager\enums\LotType.java src\com\parkinglotmanager\enums\SensorType.java
+javac -cp ".;%MYSQL_JAR%" --release 8 -Xlint:-options -d build src\com\parkinglotmanager\enums\*.java 
 if %errorlevel% neq 0 exit /b 1
 
 REM Compile utilities
@@ -32,7 +32,7 @@ if %errorlevel% neq 0 exit /b 1
 
 REM Compile models
 echo Compiling models...
-javac -cp "build;%MYSQL_JAR%" --release 8 -Xlint:-options -d build src\com\parkinglotmanager\model\User.java src\com\parkinglotmanager\model\Admin.java src\com\parkinglotmanager\model\Client.java src\com\parkinglotmanager\model\Vehicle.java src\com\parkinglotmanager\model\ParkingLot.java src\com\parkinglotmanager\model\ParkingSlot.java src\com\parkinglotmanager\model\VehicleSession.java src\com\parkinglotmanager\model\UserReport.java src\com\parkinglotmanager\model\OccupancyReport.java src\com\parkinglotmanager\model\Sensor.java src\com\parkinglotmanager\model\SensorReading.java
+javac -cp "build;%MYSQL_JAR%" --release 8 -Xlint:-options -d build src\com\parkinglotmanager\model\*.java 
 if %errorlevel% neq 0 exit /b 1
 
 REM Compile DAOs
